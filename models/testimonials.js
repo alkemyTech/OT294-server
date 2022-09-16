c651de08209a75f28f43fce7abbfdf4ce6c6704a
Juan Pablo Ramirez <pabloblog3223@gmail.com>
2022-09-16 11:44:10 -0500

    Creacion modelo, migración y controlador base de Testimonials

---
+++ models/testimonials.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+    module.exports = (sequelize, DataTypes) => {
+        class Testimonials extends Model {
+            /**
+             * Helper method for defining associations.
+             * This method is not a part of Sequelize lifecycle.
+             * The `models/index` file will call this method automatically.
+             */
+            static associate(models) {
+                // define association
+    }
+    };
+    Testimonials.init({
+        name: DataTypes.STRING,
+        image: DataTypes.STRING,
+        roleId: DataTypes.INTEGER,
+        deleteAt: DataTypes.DATE
+    }, {
+        sequelize,
+        modelName: 'Testimonials',
+    });
+    return Testimonials;
+};
