b7d63b46f14628334146f60eb3c8b000a1c16543
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-09-17 17:56:42 -0500

    Endpoint de autenticacion

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.example' });
+
+//Models
+const { User } = require('../models/user');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+/* POST users authentication. */
+router.post('/auth/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate credentials (email)
+  const user = await User.findOne({
+    where: { email, status: 'active' },
+  });
+
+  if (!user) {
+    return next(new AppError('Credentials invalid', 400));
+  }
+
+  // Validate password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return next(new AppError('Credentials invalid', 400));
+  }
+
+  // Generate JWT (JsonWebToken) ->
+  const token = await jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  // Send response
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
 });
 
 module.exports = router;
