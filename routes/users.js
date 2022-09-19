e6af7b978bb5fb34fe3fb3e1a31fee8aa6a49386
lucy1528 <luxysm@gmail.com>
2022-09-19 15:50:58 -0500

    Envio de email de bienvenida

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models')
+const { Email } = require('../utils/email.util');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/', async function(req, res, next) {
+  
+  const { firstName, lastName, email } = req.body;
+  const { user } = req;
+
+  const newUser = await db.User.create({
+    firstName,
+    lastName,
+    email,
+  });
+
+  // Send mail when user has been created
+  await new Email(user.email).sendWelcome(firstName);
+
+  res.status(201).json({
+    status: 'success',
+    newUser,
+});
+
+});
+
 
 module.exports = router;
