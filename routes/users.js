63305b09ea647c44f02b4dc6f4cb4322f063bab8
Cesar Ospina <cesarospina2000@hotmail.com>
2022-09-22 20:19:18 -0500

    [OT294-36]-Endpoint de Actualizar Usuario

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env.example' });
 const { User } = require('../models/user');
-
+const { userExists } = require('../middlewares/users.middleware');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -44,6 +44,16 @@ router.post('/auth/login', async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+/* PATCH  users updated */
+router.patch('/users/:id', userExists, async (req, res, next) => {
+  const { user } = req;
+  const { firstName, lastName, email, image, password } = req.body;
+
+  await user.update({ firstName, lastName, email, image, password });
+
+  res.status(201).json({ status: 'success', user });
 });
 
 module.exports = router;
