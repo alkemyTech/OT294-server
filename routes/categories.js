17cbae54bf845691db35e819784dbb4d53010350
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-09-22 20:36:05 -0500

    Fix minor bug

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+//Models
+const { Category } = require('../models/category')
 const { createCategoryValidators } = require('../middlewares/validators.middleware')
 
 /* GET categories listing. */
