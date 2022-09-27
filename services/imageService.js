4f1954e583a6211f7c3190d205d4745f2e8bef2d
pr4zka <92057245+pr4zka@users.noreply.github.com>
2022-09-26 22:02:09 -0400

    aws s3

--- services/imageService.js
+++ services/imageService.js
@@ -1,8 +1,32 @@
-const AWS = require('aws-sdk')
+const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3')
+const {region, Bucketname, secretAccessKey, accessKeyId} = require('./config')
+const {getSignedUrl} = require('@aws-sdk/s3-request-presigner')
 
-class imageService{
+const client = new S3Client({
+    region: region,
+    credentials: {
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey
+    }
+})
 
+
+const uploadFile = async (file) => {
+    const uploadParams = {
+        Bucket: Bucketname,
+        Key: file.originalname,
+        Body: file.buffer
+    }
+    const command = new PutObjectCommand(uploadParams)
+    return await client.send(command)
 }
 
+const getFileUrl = async (filename) => {
+  const command = new GetObjectCommand({
+    Bucket: Bucketname,
+    Key: filename
+  })
+  return await getSignedUrl(client,command, {expiresIn: 3600})
+}
 
-module.exports = imageService
+module.exports = {uploadFile, getFileUrl};
