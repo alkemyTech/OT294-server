954ced1d4faf59d951a7af79eeaab448a14af32d
pr4zka <92057245+pr4zka@users.noreply.github.com>
2022-10-03 18:31:31 -0300

    send email to new contacts

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,13 +1,14 @@
 const { catchAsync } = require("../utils/catchAsync.util");
 const { Contacts } = require("../models");
-
+const { Email } = require("../utils/email.util");
 const createContacts = catchAsync(async (req, res) => {
   const { body } = req;
   const contacts = await Contacts.create(body);
+  await new Email(body.email).sendWelcome(body.name);
   res.status(201).json({
     status: true,
     message: "Contacto registrado exitosamente",
-    data: { contacts }
+    data: { contacts },
   });
 });
 
