bc12a8b5c7711b64430e00397744c80c24a14fb1
Cesar Ospina <cesarospina2000@hotmail.com>
2022-10-02 20:37:48 -0500

    se agrega comprobacion para atributo order

--- controllers/slides.controller.js
+++ controllers/slides.controller.js
@@ -1,15 +1,40 @@
 // Utils
 const { catchAsync } = require("../utils/catchAsync.util");
+// Models
+const { Slide } = require("../models/category");
 
 const updateSlide = catchAsync(async (req, res) => {
   const { slide, file } = req;
   const { text, order, organizationId } = req.body;
   const imgRef = ref(upload, `slides/${Date.now()}_${file.originalname}`);
   const imgRes = await uploadFile(imgRef, file.buffer);
+
+  let newOrder = 0;
+  const slides = await Slide.findAll();
+
+  const entryOrder = slides.some((slide) => slide.order === order);
+  if (entryOrder) {
+    return next(new AppError("Digite un orden diferente", 404));
+  }
+
+  if ((slides.length === 0 && order === 0) || order === undefined) {
+    newOrder = 1;
+  } else {
+    if (order === 0 || order === undefined) {
+      let maximumOrder = Math.max.apply(
+        Math,
+        slides.map(function (slide) {
+          return slide.order;
+        })
+      );
+      newOrder = maximumOrder + 1;
+    }
+  }
+
   await slide.update({
     imageUrl: imgRes.metadata.fullPath,
     text,
-    order,
+    order: newOrder,
     organizationId,
   });
 
