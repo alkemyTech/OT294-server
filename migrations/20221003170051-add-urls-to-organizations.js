fd404bbb77543eed02bc32723f17fa35259d3c55
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-10-03 18:49:43 -0500

    Minor update

--- migrations/20221003170051-add-urls-to-organizations.js
+++ migrations/20221003170051-add-urls-to-organizations.js
@@ -4,12 +4,15 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return Promise.all([
       queryInterface.addColumn("Organization", "urlFacebook", {
+        allowNull: true,
         type: Sequelize.STRING
       }),
       queryInterface.addColumn("Organization", "urlLinkedIn", {
+        allowNull: true,
         type: Sequelize.STRING
       }),
       queryInterface.addColumn("Organization", "urlInstagram", {
+        allowNull: true,
         type: Sequelize.STRING
       })
     ]);
