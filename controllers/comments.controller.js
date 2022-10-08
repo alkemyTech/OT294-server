f296106081024bfe0af90acf804d300cb833fe61
Cesar Ospina <cesarospina2000@hotmail.com>
2022-10-07 22:02:33 -0500

    [OT294-83]-Endpoint Actualización Comentarios

---
+++ controllers/comments.controller.js
@@ -0,0 +1,27 @@
+const { catchAsync } = require("../utils/catchAsync.util");
+
+const updateComment = catchAsync(async (req, res) => {
+  const { comment, sessionUser } = req;
+  const { body } = req.body;
+
+  if (sessionUser.id !== comment.userId || sessionUser.roleId !== 0) {
+    return next(
+      new AppError(
+        "Usted no tiene permisos para actualizar este comentario",
+        404
+      )
+    );
+  }
+
+  await comment.update({ body });
+
+  res.json({
+    status: true,
+    message: "El comentario ha sido actualizado",
+    data: comment,
+  });
+});
+
+module.exports = {
+  updateComment,
+};
