b6504304949d14a37bc536f08c942b2caa8ea2ff
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-10-07 19:42:53 -0500

    [OT294-78]-Crear seed de Usuarios

--- README.md
+++ README.md
@@ -26,3 +26,69 @@ npx sequelize-cli db:seed:all
 ``` bash
 npm start
 ```
+
+```
+Listado de usuarios
+<!-- Usuarios administradores con ROLE = 0 -->
+{email: testuseralkemy1@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy2@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy3@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy4@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy5@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy6@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy7@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy8@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy9@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy10@yopmail.com,
+password: pass1234
+}
+<!-- Usuarios estandar o regulares con ROLE = 1 -->
+{email: testuseralkemy11@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy12@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy13@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy14@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy15@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy16@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy17@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy18@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy19@yopmail.com,
+password: pass1234
+},
+{email: testuseralkemy20@yopmail.com,
+password: pass1234
+}
+```
---
+++ seeders/20221007192053-create-users.js
@@ -0,0 +1,238 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert("Users", [
+    {firstName: "Usuario",
+      lastName: "Numero 1",
+      email: "testuseralkemy1@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 2",
+      email: "testuseralkemy2@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 3",
+      email: "testuseralkemy3@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 4",
+      email: "testuseralkemy4@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 5",
+      email: "testuseralkemy5@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 6",
+      email: "testuseralkemy6@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 7",
+      email: "testuseralkemy7@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 8",
+      email: "testuseralkemy8@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 9",
+      email: "testuseralkemy9@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 10",
+      email: "testuseralkemy10@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 0,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 11",
+      email: "testuseralkemy11@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 12",
+      email: "testuseralkemy12@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 13",
+      email: "testuseralkemy13@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 14",
+      email: "testuseralkemy14@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 15",
+      email: "testuseralkemy15@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 16",
+      email: "testuseralkemy16@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 17",
+      email: "testuseralkemy17@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 18",
+      email: "testuseralkemy18@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 19",
+      email: "testuseralkemy19@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {firstName: "Usuario",
+      lastName: "Numero 20",
+      email: "testuseralkemy20@yopmail.com",
+      // Important: Password not encrypted yet!
+      password: "pass1234",
+      status: "active",
+      roleId: 1,
+      image: "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+
+], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete("People", null, {});
+     */
+  }
+};
