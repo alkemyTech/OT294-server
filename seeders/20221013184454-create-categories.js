82094ecdff483c6df9ec8908498d6c9f956178b4
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-10-17 13:53:31 -0500

    [OT294-109]-Crear seed de Categorias

---
+++ seeders/20221013184454-create-categories.js
@@ -0,0 +1,226 @@
+"use strict";
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert("Categories", [
+            {
+                name: "Image 1",
+                description: "Imagen 1",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 2",
+                description: "Imagen 2",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 3",
+                description: "Imagen 3",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 4",
+                description: "Imagen 4",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 5",
+                description: "Imagen 5",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 6",
+                description: "Imagen 6",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 7",
+                description: "Imagen 7",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 8",
+                description: "Imagen 8",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 9",
+                description: "Imagen 9",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 10",
+                description: "Imagen 10",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 11",
+                description: "Imagen 11",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 12",
+                description: "Imagen 12",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 13",
+                description: "Imagen 13",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 14",
+                description: "Imagen 14",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 15",
+                description: "Imagen 15",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 16",
+                description: "Imagen 16",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 17",
+                description: "Imagen 17",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 18",
+                description: "Imagen 18",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 19",
+                description: "Imagen 19",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 20",
+                description: "Imagen 20",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 21",
+                description: "Imagen 21",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 22",
+                description: "Imagen 22",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 23",
+                description: "Imagen 23",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 24",
+                description: "Imagen 24",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 25",
+                description: "Imagen 25",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 26",
+                description: "Imagen 26",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 27",
+                description: "Imagen 27",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 28",
+                description: "Imagen 28",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 29",
+                description: "Imagen 29",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                name: "Image 30",
+                description: "Imagen 30",
+                image: "none",
+                createdAt: new Date,
+                updatedAt: new Date
+            }], {});
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete("People", null, {});
+         */
+    }
+};
